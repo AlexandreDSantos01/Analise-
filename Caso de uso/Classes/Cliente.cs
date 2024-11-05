1fc52f3653f85b8ed9bd4a8276dbb6a078a16a0d
AlexandreDSantos01 <Alexandreds4@hotmail.com>
2024-11-04 23:20:53 -0300

    Commit 2.6

---
+++ Caso de uso/Classes/Cliente.cs
@@ -0,0 +1,32 @@
+namespace Classes
+{
+	public class Cliente
+	{
+		private int Id;
+
+		private string Nome;
+
+		private string Cpf;
+
+		private string Telefone;
+
+		private string Email;
+
+		private string Rua;
+
+		private int NRua;
+
+		private string Bairro;
+
+		private string Cidade;
+
+		private string Estado;
+
+		private string Cep;
+
+		private string Situacao;
+
+	}
+
+}
+
---
+++ Caso de uso/Classes/Usuario.cs
@@ -0,0 +1,38 @@
+namespace Classes
+{
+	public class Usuario
+	{
+		public int Id;
+
+		public string Cpf;
+
+		public string Nome;
+
+		public string Telefone;
+
+		public string Email;
+
+		public string Rua;
+
+		public int NRua;
+
+		public string Bairro;
+
+		public string Cidade;
+
+		public string Estado;
+
+		public string Cep;
+
+		public string TipoUsuario;
+
+		public string Situacao;
+
+		protected string Ulogar;
+
+		protected string Senha;
+
+	}
+
+}
+
